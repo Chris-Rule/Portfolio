5343006b427f419ed5b8521ee1a27bdbc38c6648
Chris-Rule <rule-@hotmail.com>
2022-10-24 14:45:43 +0100

    Added extra fields to new input form and implemented redirects

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -16,6 +16,16 @@ const Contact = () => {
           <input type="text" name="first-name" id="name" />
         </div>
 
+        <div>
+          <label htmlFor="email"> Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+
+        <div>
+          <label htmlFor="message"> Message</label>
+          <textarea type="message" name="message" id="message" />
+        </div>
+
         <button type="submit">Submit the results</button>
       </form>
     </div>
