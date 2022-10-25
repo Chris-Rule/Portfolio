94e92ae4ec3a3cf1ddbc1ae4b29c073b1d9b2692
Chris-Rule <rule-@hotmail.com>
2022-10-25 16:27:06 +0100

    updated styling of contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,43 +1,50 @@
 import "./Contact.css";
-
+import GithubIcon from "../Icons/GithubIcon";
+import LinkedInIcon from "../Icons/LinkedInIcon";
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-page">
+      <section className="social-links">
+        <GithubIcon />
+        <h2>Contact Form</h2>
+        <LinkedInIcon />
+      </section>
       <form
         name="contact v2"
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         onSubmit="submit"
+        className="contact-panel"
       >
         <input type="hidden" name="form-name" value="contact v1" />
-
         <div hidden>
           <input name="bot-field" />
         </div>
 
         <div>
           <label>
-            Name
+            Input &gt; Name
             <input type="text" name="first-name" />
           </label>
         </div>
 
         <div>
           <label>
-            Email
+            Input &gt; Email
             <input type="email" name="email" />
           </label>
         </div>
 
         <div>
           <label>
-            Message
-            <textarea type="message" name="message"></textarea>
+            Input &gt; Message
+            <textarea type="message" name="message" rows="5"></textarea>
           </label>
         </div>
-
-        <button type="submit">Submit the results</button>
+        <div className="button-container">
+          <button type="submit">Send</button>
+        </div>
       </form>
     </div>
   );
